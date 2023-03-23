863a382376a47a91f04c4ca712570c5ba3e59bb7
Alexandre Ferreira de Lima <alexandre.ferreira1445@gmail.com>
2023-03-23 16:06:51 -0300

    🔨 Change path from config files

--- config/config.py
+++ config/config.py
@@ -6,8 +6,8 @@ from dynaconf import Dynaconf
 settings = Dynaconf(
     envvar_prefix="DYNACONF",
     settings_files=[
-        './settings.toml',
-        './.secrets.toml'
+        'settings.toml',
+        '.secrets.toml'
     ],
 )
 
