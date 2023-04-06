8a3f95bb014466a3726545c77135888d6faaa363
Alexandre Ferreira de Lima <alexandre.ferreira1445@gmail.com>
2023-04-06 08:56:53 -0300

    ⚠️ Remove unnecessary if

--- src/utils/zip_utils.py
+++ src/utils/zip_utils.py
@@ -5,8 +5,7 @@ from src.utils import has_file, remove
 def check_if_has_unziped(file_exe, file_zip_path, extract_path):
     if not has_file(file_exe):
         zip_file_path = file_zip_path
-        if has_file(zip_file_path):
-            unzip_file(zip_file_path, extract_path)
+        unzip_file(zip_file_path, extract_path)
 
 
 def unzip_file(zip_file_path, extract_path):
